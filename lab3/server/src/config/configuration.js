65a6b8970450087c308198bb049f85ef7e8d717a
Viacheslav Filipenko <filipenko.slavik@gmail.com>
2020-06-03 11:25:34 +0300

    update config

--- lab3/server/src/config/configuration.js
+++ lab3/server/src/config/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   database: {
     url: process.env.MONGO_URL || "mongodb://localhost:27017/minutemedia",
   },
